d2f65958454fed207056493ef072262452d71bbc
akki64126 <98653823+akki64126@users.noreply.github.com>
2022-02-03 12:15:53 +0530

    Update luhn.py

--- luhn.py
+++ luhn.py
@@ -2,26 +2,30 @@ def luhn(cardno):
      
     no_of_digits = len(cardno)
     digit_sum = 0
-    isSecond = False
+    value = False
+    if no_of_digits>13 and no_of_digits<=16:
      
-    for i in range(no_of_digits - 1, -1, -1):
-        d = ord(cardno[i]) - ord('0')
-     
-        if (isSecond == True):
-            d = d * 2
-        digit_sum += d // 10
-        digit_sum += d % 10
-  
-        isSecond = not isSecond
-     
-    if (digit_sum % 10 == 0):
-        return True
+        for i in range(no_of_digits - 1, -1, -1):
+            d = ord(cardno[i]) - ord('0')
+        
+            if (value == True):
+                d = d * 2
+            digit_sum += d // 10
+            digit_sum += d % 10
+    
+            value = not value
+        
+        if (digit_sum % 10 == 0):
+            return True
+        else:
+            return False
     else:
+    
         return False
   
 if __name__=="__main__":
      
-    cardno = input("Enter your credit card number..")
+    cardno = input()
      
     if (luhn(cardno)):
         print("This is a valid credit card")
